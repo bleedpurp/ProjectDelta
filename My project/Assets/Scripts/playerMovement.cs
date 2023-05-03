80f237116c14372b1e47bf0146437657b3ad828e
BleedPurp <cmccarthy1231@gmail.com>
2023-05-03 00:00:45 -0400

    added player model

---
+++ My project/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f; 
+    private Rigidbody2D rb; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        Vector2 movement = new Vector2(horizontalInput, verticalInput);
+
+        rb.velocity = movement.normalized * moveSpeed * Time.deltaTime; 
+    }
+}
